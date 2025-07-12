7622203c1ad7f7e9089873654facdf0360563b8a
princesslala99 <naepriki@gmail.com>
2025-07-12 08:51:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,6 @@ st.write("Hello, *World!* :sunglasses:")
 st.title("This is a title")
 st.header("_Streamlit_ is :blue[cool] :sunglasses:")
 st.subheader("_Streamlit_ ist.subheader("_Streamlit_ is :blue[cool] :sunglasses:")s :blue[cool] :sunglasses:")
+
 st.markdown("Here's a bouquet &mdash;\
             :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")
